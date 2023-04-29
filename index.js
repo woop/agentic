1f6aaa74a4f96380d04837f5553c3ba2818c9017
Willem Pienaar <github@willem.co>
2023-04-29 13:58:40 -0700

    feat(index.js): add support for establishing a network connection to a remote enigma server feat(index.js): add support for sending encrypted data over the network feat(index.js): add support for closing a network connection to a remote enigma server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const myst = require("myst_internal");
+const crypto = require("crypto");
+const net = require("net");
 
 function createEnigma() {
   return new Enigma();
@@ -7,10 +9,48 @@ function createEnigma() {
 class Enigma {
   constructor() {
     this.core = myst();
+    this.connection = null;
+    this.socket = null;
+    this.algorithm = 'aes-256-ctr';
+    this.key = crypto.randomBytes(32);
   }
 
   summon() {
     return new Agent(this.core);
+  }
+
+  establishConnection(port, host) {
+    this.socket = new net.Socket();
+    this.socket.connect(port, host, () => {
+      console.log('Connected to remote enigma server');
+    });
+
+    this.socket.on('data', (data) => {
+      const decipher = crypto.createDecipheriv(this.algorithm, this.key, data.slice(0, 16));
+      const decryptedData = Buffer.concat([decipher.update(data.slice(16)), decipher.final()]);
+      console.log('Received data:', decryptedData.toString());
+    });
+
+    this.socket.on('close', () => {
+      console.log('Connection closed');
+    });
+  }
+
+  sendData(data) {
+    if (!this.socket) {
+      throw new Error('No connection established');
+    }
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(this.algorithm, this.key, iv);
+    const encryptedData = Buffer.concat([cipher.update(data), cipher.final()]);
+    this.socket.write(Buffer.concat([iv, encryptedData]));
+  }
+
+  closeConnection() {
+    if (this.socket) {
+      this.socket.destroy();
+      this.socket = null;
+    }
   }
 }
 
@@ -21,6 +61,18 @@ class Agent {
 
   engage() {
     this.core.activate();
+  }
+
+  connect(port, host) {
+    this.core.establishConnection(port, host);
+  }
+
+  transmit(data) {
+    this.core.sendData(Buffer.from(data));
+  }
+
+  disconnect() {
+    this.core.closeConnection();
   }
 }
 
